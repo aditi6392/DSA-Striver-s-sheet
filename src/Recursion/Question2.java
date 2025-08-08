0a3f3bcda6cfbaa12c432f00b816ff9527ac899d
aditi <aditi27.me@gmail.com>
2025-08-08 13:40:51 +0530

    name print n times using recursion

--- src/Recursion/Question2.java
+++ src/Recursion/Question2.java
@@ -1,16 +1,22 @@
 package Recursion;
+//Printing names n times
+
+
+import java.util.Scanner;
+
 
 public class Question2 {
-    int count =0;
-    public void name(){
-        if(count==5) return;
+    public void name(int i,int n){
+        if(i>n) return;
+
         System.out.println("Aditi");
-        count++;
-        name();
+        name(i+1,n);    //RECURSION
     }
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
         Question2 q2=new Question2();
-        q2.name();
+        q2.name(1,n);
     }
 }
